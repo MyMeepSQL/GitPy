7ae9f0c1aceb684b677478995e749846164a88db
Thomas Pellissier <82945541+MyMeepSQL@users.noreply.github.com>
2023-04-05 10:14:26 +0200

    Update send_email.py

--- src/core/send_email.py
+++ src/core/send_email.py
@@ -100,9 +100,9 @@ def send_email():
         smtp_server = config.get(section, 'smtp_server')
         smtp_port = config.get(section, 'smtp_port')
         smtp_username = config.get(section, 'smtp_username')
-        smtp_password_env_var_name = os.environ[config.get(section, 'smtp_password')]
-        print(smtp_password_env_var_name)
-        smtp_password = os.environ[smtp_password_env_var_name]
+        # smtp_password_env_var_name = os.environ[config.get(section, 'smtp_password')]
+        # print(smtp_password_env_var_name)
+        smtp_password = os.environ[config.get(section, 'smtp_password')]
         print(smtp_password)
         receiver_email = config.get(section, 'receiver_email')
         github_repo_name = config.get(section, 'github_repo_name')
